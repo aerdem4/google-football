8d3beb304588ea384b151e71fa9ed25d9eee8a72
aerdem4 <ahmeterd4@gmail.com>
2020-10-08 08:49:45 +0300

    v003

--- agent.py
+++ agent.py
@@ -7,7 +7,6 @@ import utils
 class Agent:
     def __init__(self):
         self.gc = GameCache()
-        self.just_got_the_ball = 0
         self.opponent_goal = [1.0, 0]
         self.opponent_penalty = [0.8, 0]
         self.own_goal = [-1.0, 0]
@@ -25,44 +24,59 @@ class Agent:
         opponent = [i for i, r in enumerate(self.current_obs["right_team_roles"]) if r == role][0]
         return np.array(self.current_obs["right_team"])[opponent]
 
+    def _get_closest_opponent(self, point):
+        distances = [utils.distance(opp, point) for opp in self.current_obs["right_team"]]
+        closest = np.argmin(distances)
+        return distances[closest], self.current_obs["right_team"][closest]
+
     def defend(self):
-        self.just_got_the_ball = 0
-        controlled_player_pos = self.current_obs['left_team'][self.current_obs['active']]
         if Action.Sprint not in self.current_obs['sticky_actions']:
             return Action.Sprint
 
-        # ball is far
-        if self.current_obs['ball'][0] - controlled_player_pos[0] < -0.1:
-            self._run_towards(controlled_player_pos, self.own_goal)
+        closest_opp_dist, closest_opp = self._get_closest_opponent(self.controlled_player_pos)
+        if utils.distance(self.gc.ball[-1], self.controlled_player_pos) + 0.01 < closest_opp_dist < 0.05:
+            return Action.Slide
 
-        return self._run_towards(controlled_player_pos, self.current_obs['ball'])
+        # ball is far
+        if self.gc.ball[-1][0] - self.controlled_player_pos[0] < -0.1:
+            self._run_towards(self.controlled_player_pos, self.own_goal)
+
+        return self._run_towards(self.controlled_player_pos, self.gc.ball[-1] + 2*self.gc.get_ball_speed())
 
     def attack(self):
-        opp_gk = self._get_opponent_by_role(PlayerRole.GoalKeeper)
+        direction = self.current_obs["left_team_direction"][self.current_obs["active"]]
 
+        opp_gk = self._get_opponent_by_role(PlayerRole.GoalKeeper)
         dist_to_goal = utils.distance(self.controlled_player_pos, self.opponent_goal)
         dist_to_gk = utils.distance(self.controlled_player_pos, opp_gk)
         if (dist_to_goal < 0.3) or (dist_to_gk < 0.3):
             return Action.Shot
 
-        self.just_got_the_ball += 1
-        if self.just_got_the_ball == 5:
-            if self.controlled_player_pos[0] < -0.2:
+        if self.gc.time_since_ball == 5:
+            if self.controlled_player_pos[0] < -0.4:
                 return Action.HighPass
+            elif direction[0] < 0 and abs(direction[0]) > abs(direction[1]):
+                return Action.ShortPass
 
-        if self.just_got_the_ball > 1:
+        if self.gc.time_since_ball > 1:
             if Action.Sprint not in self.current_obs['sticky_actions']:
                 return Action.Sprint
 
         return self._run_towards(self.controlled_player_pos, self.opponent_penalty)
 
     def act(self, obs):
+        #try:
+        self.gc.update(obs)
         self.current_obs = obs
         self.controlled_player_pos = obs['left_team'][obs['active']]
 
         if obs["game_mode"] == GameMode.Penalty:
             return Action.Shot
-        if obs['ball_owned_player'] == obs['active'] and obs['ball_owned_team'] == 0:
+        if obs["game_mode"] == GameMode.Corner:
+            return Action.HighPass
+        if self.gc.attacking[-1]:
             return self.attack()
         else:
             return self.defend()
+        #except:
+        #    return Action.Shot
--- game_cache.py
+++ game_cache.py
@@ -1,7 +1,35 @@
+import numpy as np
+
+
 class GameCache:
     def __init__(self):
         self.time = 0
-        self.last_time_with_ball = 0
+        self.ball = []
+        self.controlled_player = []
+        self.attacking = []
+        self.time_since_ball = 0
+
+    def _get_speed(self, obj_array):
+        if len(obj_array) < 2:
+            return np.array([0, 0])
+        return obj_array[-1] - obj_array[-2]
 
     def update(self, obs):
-        pass
+        self.time += 1
+        self.ball.append(np.array(obs["ball"][:2]))
+        self.attacking.append((obs['ball_owned_player'] == obs['active']) and (obs['ball_owned_team'] == 0))
+        if self.attacking[-1]:
+            if self.time_since_ball == 0:  # gained the ball
+                self.controlled_player = []
+            self.time_since_ball += 1
+        else:
+            if self.time_since_ball > 0:  # lost the ball
+                self.controlled_player = []
+            self.time_since_ball = 0
+        self.controlled_player.append(np.array(obs['left_team'][obs['active']]))
+
+    def get_ball_speed(self):
+        return self._get_speed(self.ball)
+
+    def get_player_speed(self):
+        return self._get_speed(self.controlled_player)
