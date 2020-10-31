251d8d2e909ed2bde0f39dd229248e3ce4bc411c
aerdem4 <ahmeterd4@gmail.com>
2020-10-31 11:31:12 +0100

    v024: change clear ball behavior

--- agent.py
+++ agent.py
@@ -49,12 +49,16 @@ class Agent:
             return True
         return False
 
-    def _tactic_foul(self):
-        ball_to_own_goal = utils.distance(self.gc.ball[-1], self.own_goal)
+    def _last_man_standing(self):
+        dist = utils.distance(self.gc.controlled_player.pos, self.own_goal)
         distances = [utils.distance(player.pos, self.own_goal) for player in self.gc.players[OWN_TEAM]
                      if player.role != PlayerRole.GoalKeeper]
+        if sum([d < dist for d in distances]) == 0:
+            return True
+        return False
 
-        if sum([d < ball_to_own_goal - 0.02 for d in distances]) > 0:
+    def _tactic_foul(self):
+        if not self._last_man_standing():
             return None
 
         closest_opp_dist, closest_opp = self._get_closest(self.gc.controlled_player.pos, OPP_TEAM)
@@ -68,16 +72,35 @@ class Agent:
         return None
 
     def _decide_clear_ball(self):
-        if (self.gc.ball[-1][0] < -0.7) and (self.action_counter[Action.Shot] > 19):
-            return Action.Shot
-        if (self.gc.ball[-1][0] < -0.3) and (self.action_counter[Action.HighPass] > 19):
+        dist = utils.distance(self.own_goal, self.gc.ball[-1])
+        danger_zone = dist < 0.5
+        if not danger_zone and not self._last_man_standing():
+            return None
+
+        if self.gc.controlled_player.direction[0] > 0 and self.action_counter[Action.HighPass] > 9:
             return Action.HighPass
+
+        closest_opp_dist, closest_opp = self._get_closest(self.gc.controlled_player.pos, OPP_TEAM)
+        target_dir = self._run_towards(self.gc.controlled_player.pos, self.opponent_penalty)
+        if closest_opp_dist > 0.06:
+            return target_dir
+
+        opp_between = utils.between(closest_opp.pos, self.gc.controlled_player.pos, self.opponent_penalty, -0.5)
+        if opp_between:
+            direction = self.gc.controlled_player.pos - closest_opp.pos
+            direction /= utils.length(direction)
+            current_dir = self.gc.controlled_player.direction / utils.length(self.gc.controlled_player.direction)
+            direction += current_dir
+            if direction[1] > 0:
+                return Action.Bottom
+            else:
+                return Action.Top
         return None
 
     def defend(self):
         ball_coming = False
+        ball_dist_now = utils.distance(self.gc.controlled_player.pos, self.gc.ball[-1])
         if len(self.gc.ball) > 1:
-            ball_dist_now = utils.distance(self.gc.controlled_player.pos, self.gc.ball[-1])
             ball_dist_prev = utils.distance(self.gc.controlled_player.pos, self.gc.ball[-2])
             ball_coming = 0.1 < ball_dist_now < ball_dist_prev
         if Action.Sprint not in self.gc.sticky_actions and not ball_coming:
@@ -85,9 +108,7 @@ class Agent:
         if Action.Sprint in self.gc.sticky_actions and ball_coming and self.gc.neutral_ball:
             return Action.ReleaseSprint
 
-        time_projection = 2
-        if self.gc.ball[-1][0] < self.gc.controlled_player.pos[0]:
-            time_projection = 9
+        time_projection = 7
         dir_action = self._run_towards(self.gc.controlled_player.pos,
                                        self.gc.ball[-1] + time_projection * self.gc.get_ball_speed())
         if not self.gc.neutral_ball:
@@ -102,10 +123,6 @@ class Agent:
                     return Action.Idle
                 else:
                     return action
-
-            clear_action = self._decide_clear_ball()
-            if clear_action is not None and (dir_action in self.gc.sticky_actions):
-                return clear_action
 
         return dir_action
 
@@ -125,7 +142,7 @@ class Agent:
 
         clear_action = self._decide_clear_ball()
         if clear_action is not None:
-            return self.macro_list.add_macro([Action.Right, clear_action], True)
+            return clear_action
 
         opp_gk = self._get_opponent_by_role(PlayerRole.GoalKeeper)
         dist_to_goal = utils.distance(self.gc.controlled_player.pos, self.opp_goal)
